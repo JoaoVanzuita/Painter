e0ddd8fbe25a7692063e2d7aee4f8adf6489ee08
João Vanzuita <joaog17vanzuita@gmail.com>
2022-04-03 16:17:56 -0300

    Code improvements

--- main.js
+++ main.js
@@ -1,37 +1,50 @@
-function criarCores(){
+function criarDivsPaleta() {
 
-  let cores = ["black","blue","purple", "yellow", "red", "green", "white"];
+    let cores = ["black", "blue", "purple", "yellow", "red", "green", "white"];
 
-  let divPaleta = document.getElementById('paleta');
+    let divPaleta = document.getElementById('paleta');
 
-  for(i=0; i < cores.length; i++){
+    cores.forEach(cor => {
 
-    let divCores = document.createElement('div');
+        let divCores = document.createElement('div');
 
-    divCores.innerHTML =`<div class="cores" style="background-color:${cores[i]}"></div>`;
+        divCores.innerHTML = `<div class="cores" style="background-color:${cor}"></div>`;
 
-    divPaleta.appendChild(divCores);
+        divPaleta.appendChild(divCores);
 
-  }
-
-}
-
-function criarDivsDesenho(){
-
-  let html = "<div class='espaco'></div>";
-  let divDesenho = document.getElementById('desenho');
-
-  for(i=0; i < 1081; i++){
-
-
-    let divEspaco = document.createElement('div');
-    divEspaco.innerHTML = html;
-
-    divDesenho.appendChild(divEspaco);
-
-  }
+    })
 
 }
 
-criarCores()
+function criarDivsDesenho() {
+
+    let html = "<divp class='espaco'></divp>";
+    let divDesenho = document.getElementById('desenho');
+
+    for (i = 0; i < 1081; i++) {
+
+        let divEspaco = document.createElement('div');
+        divEspaco.innerHTML = html;
+
+        divDesenho.appendChild(divEspaco);
+
+    }
+
+}
+
+criarDivsPaleta()
 criarDivsDesenho()
+
+function cliquePaleta() {
+
+
+    //ao clicar na div cores, pegar background color do target e guardar em uma variável corAtual
+    //colocar borda amarela na cor clicada
+}
+
+
+function cliqueDesenho() {
+
+    //ao clicar em uma div do desenho, pegar a cor armazenada na variável corAtual e colocar no background color do target
+
+}
